5f36b0a0493f48a13740075f508e6075ebb2770b
김다슬 <daseul727@gmail.com>
2024-02-05 11:40:02 +0900

    init dialog

--- app/src/main/java/com/example/jointest/MainActivity.kt
+++ app/src/main/java/com/example/jointest/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.example.jointest
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
+import com.example.jointest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private lateinit var spinner: Spinner
     private lateinit var contryCode: TextView
     private lateinit var phoneNumber: EditText
+
+    private lateinit var agreePersonal: Button
+    private lateinit var agreeGps: Button
+    private lateinit var agreeThird: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // init
         spinner = findViewById(R.id.contryName)
@@ -39,5 +51,35 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // 3. 이용약관 팝업 이벤트
+        agreePersonal = findViewById(R.id.agreePersonal)
+        agreeGps = findViewById(R.id.agreeGps)
+        agreeThird = findViewById(R.id.agreeThird)
+
+        agreePersonal.setOnClickListener {
+            showAlertDialog("개인정보보호약관", R.layout.agree_personal_modal)
+        }
+
+        agreeGps.setOnClickListener {
+            showAlertDialog("위치기반 서비스약관동의", R.layout.agree_gps_modal)
+        }
+
+        agreeThird.setOnClickListener {
+            showAlertDialog("제3자 정보제공동의", R.layout.agree_third_modal)
+        }
     }
+    private fun showAlertDialog(title: String, layoutId: Int) {
+        val mDialogView = LayoutInflater.from(this).inflate(layoutId, null)
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+            .setTitle(title)
+
+        val mAlertDialog = mBuilder.show()
+
+        val noButton = mDialogView.findViewById<Button>(R.id.closeButton)
+        noButton.setOnClickListener {
+            mAlertDialog.dismiss()
+        }
+    }
+
 }
